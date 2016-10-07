aafbc035ed06cf7050abaaba9f2fc50e92a56a13
Kenaz Kwa <kenazk@kenaz2.corp.puppetlabs.net>
2016-10-06 17:35:18 -0700

    Adding real custom fact

--- site/profile/facts.d/azure.ps1
+++ site/profile/facts.d/azure.ps1
@@ -1,1 +1,12 @@
-Write-Host key3=val3
+# Get the SMBIOS Asset Tag
+$systemEnclosure = Get-WmiObject -class Win32_SystemEnclosure -namespace root\CIMV2
+
+# Compares tag to known value
+if ($systemEnclosure.SMBIOSAssetTag -eq "7783-7084-3265-9085-8269-3286-77")
+{
+  Write-Host "RunningOnAzure=true"
+}
+else
+{
+  Write-Host "RunningOnAzure=False"
+}
