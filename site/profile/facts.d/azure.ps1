5a8c52219144cd9265db01b16221a8a75048911e
Kenaz Kwa <kenazk@kenaz2.corp.puppetlabs.net>
2016-10-06 17:36:45 -0700

    Fixing custom fact

--- site/profile/facts.d/azure.ps1
+++ site/profile/facts.d/azure.ps1
@@ -4,9 +4,9 @@ $systemEnclosure = Get-WmiObject -class Win32_SystemEnclosure -namespace root\CI
 # Compares tag to known value
 if ($systemEnclosure.SMBIOSAssetTag -eq "7783-7084-3265-9085-8269-3286-77")
 {
-  Write-Host "RunningOnAzure=true"
+  Write-Host "azure_vm=true"
 }
 else
 {
-  Write-Host "RunningOnAzure=False"
+  Write-Host "azure_vm=False"
 }
