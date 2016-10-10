b323cb108885fc39ebf9cb1ec27027b9f982ae72
Kenaz Kwa <kenazk@kenaz2.corp.puppetlabs.net>
2016-10-10 11:21:57 -0700

    Adding back iis resource

--- site/profile/manifests/iis/baseline.pp
+++ site/profile/manifests/iis/baseline.pp
@@ -2,29 +2,22 @@ class profile::iis::baseline (
   $root_iis_path  = hiera('profile::iis::baseline::root_iis_path','C:/inetpub'),
 ) {
 
-  # Install Dot Net 4.5 first
-#  winfeature{'NET-Framework-45-Core,NET-Framework-45-ASPNET':
-#     ensure => 'present',
-#  } ->
-  # Install IIS and dependent features
-#  winfeature{'Web-Server,Web-WebServer':
-#     ensure => 'present',
-#  }
-
   $dotnet_features = ['NET-Framework-45-Core','NET-Framework-45-ASPNET']
   $iis_features = ['Web-Server','Web-WebServer','Web-Mgmt-Console','Web-Mgmt-Tools']
 
+  # Install Dot Net 4.5 first
   windowsfeature { $dotnet_features:
     ensure => present,
   } ->
+  # Install IIS and dependent features
   windowsfeature { $iis_features:
     ensure => present,
   } ->
   # Stop the Default Website
-#  iis_site { 'Default Web Site':
-#     ensure   => 'stopped',
-#  }
-
+  iis_site { 'Default Web Site':
+     ensure   => 'stopped',
+  }
+  # Ensure IIS root path exists
   file { $root_iis_path:
     ensure => 'directory',
   }
