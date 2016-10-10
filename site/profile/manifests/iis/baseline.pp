dd85eb0ce7f244deabc44d22b5340d644a1019c1
Kenaz Kwa <kenazk@kenaz2.corp.puppetlabs.net>
2016-10-10 11:35:49 -0700

    Fixing packages

--- site/profile/manifests/iis/baseline.pp
+++ site/profile/manifests/iis/baseline.pp
@@ -2,13 +2,13 @@ class profile::iis::baseline (
   $root_iis_path  = hiera('profile::iis::baseline::root_iis_path','C:/inetpub'),
 ) {
 
-  $dotnet_features = ['NET-Framework-45-Core','NET-Framework-45-ASPNET']
-  $iis_features = ['Web-Server','Web-WebServer','Web-Asp-Net45']
+  $dotnet_features = ['NET-Framework-45-Core']
+  $iis_features = ['Web-Server','Web-Asp-Net45']
 
   # Install Dot Net 4.5 first
-  windowsfeature { $dotnet_features:
-    ensure => present,
-  } ->
+  #windowsfeature { $dotnet_features:
+  #  ensure => present,
+  #} ->
   # Install IIS and dependent features
   windowsfeature { $iis_features:
     ensure => present,
