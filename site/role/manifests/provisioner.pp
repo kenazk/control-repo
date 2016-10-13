d3ec118b03f824a01f4df15c960a1baf1bd953dd
Kenaz Kwa <kenazk@kenaz2.local>
2016-10-13 13:10:30 -0700

    Removing azure specific bits

--- site/role/manifests/provisioner.pp
+++
@@ -1,6 +0,0 @@
-class role::provisioner {
-
-  #This role would be made of all the profiles that need to be included to make a webserver work
-  #All roles should include the base profile
-  include profile::azure::vmss
-}
