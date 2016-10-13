4987999985204c56f7e549190d3ff00828964d8b
Kenaz Kwa <kenazk@kenaz2.local>
2016-10-13 10:58:55 -0700

    Fixing typos

--- site/role/manifests/provisioner.pp
+++ site/role/manifests/provisioner.pp
@@ -2,5 +2,5 @@ class role::provisioner {
 
   #This role would be made of all the profiles that need to be included to make a webserver work
   #All roles should include the base profile
-  include profile::azure::vmss.pp
+  include profile::azure::vmss
 }
